509f5e228e12e908c982ec7e536c3e598f688b1c
Randall Randall <randall@randallsquared.com>
2014-01-29 21:34:51 -0500

    Update globals-are-evil.js
    Stop coalbin from being global.

--- globals-are-evil.js
+++ globals-are-evil.js
@@ -1,18 +1,25 @@
 var clothesAreClean = true;
-var coalbin = [];
 var mountain = ['o','o','o','o','o','o','o','o','o','o','o','o','o','o']; // <== pieces of coal!
 var amountOfCoal = 0;
 var coalScoopCapacity = 3;
 
 var startmining = function() {
+  var coalbin = [];
+  var theCoal = [];
   while(mountainHasCoal()) {
     if(haveCleanClothes()) {
-      doSomeMining();
+      theCoal = doSomeMining();
+      for(var i = 0; i<theCoal.length; i++)
+      {
+          coalbin.push(theCoal[i]);
+      }
+      theCoal = [];
     } else {
       console.log("Need to wash clothes!");
-      washClothes();
+      coalbin = washClothes(coalbin);
     }
   }
+  return coalbin.length;
 };
 
 var mountainHasCoal = function() {
@@ -24,23 +31,25 @@ var haveCleanClothes = function() {
 };
 
 var doSomeMining = function() {
+  var coalMined = []
   amountOfCoal = Math.ceil(Math.random()*coalScoopCapacity);
   for(var i=0; i<amountOfCoal; i++) {
     var coal = mountain.pop();
-    coalbin.push(coal);
+    coalMined.push(coal);
   }
+  return coalMined;
 };
 
-var washClothes = function() {
-  if(coalbin.pop()) {
+var washClothes = function(myCoalbinNotYours) {
+  if(myCoalbinNotYours.pop()) {
     clothesAreClean = true;
   } else {
     // oh, no, we don't have enough coal to run the washer!
-    throw new Error("Washing Machine Error: code 729, insufficient power");  
+    throw new Error("Washing Machine Error: code 729, insufficient power");
     // ^^^ this is a joke; don't write confusing error messages like this
   }
-  return true; 
+  return myCoalbinNotYours;
 };
 
-startmining();
-console.log("I mined "+coalbin.length+" pieces of coal from that bad ol' mountain!");
+var amountMined = startmining();
+console.log("I mined "+amountMined+" pieces of coal from that bad ol' mountain!");
